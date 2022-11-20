8e7fe247fc8b54d7e199661a4cad82241f7d915b
kaitoohsiro <kaitooshiro7@gmail.com>
2022-11-20 14:57:44 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-sharing-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
